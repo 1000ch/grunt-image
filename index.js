53bc177a30dc21e84e43e02bd86427ccd19f31ba
1000ch <orz1000ch@gmail.com>
2014-01-24 13:54:57 +0900

    add files

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function ImageOptimize() {
+}
+
+ImageOptimize.prototype.optimizeJpg = function() {};
+
+ImageOptimize.prototype.optimizePng = function() {};
+
+ImageOptimize.prototype.optimizeGif = function() {};
+
+module.exports = ImageOptimize;
