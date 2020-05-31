1a91365b939782a39d99fb108f29cfd5c1348958
1000ch <shogosensui@gmail.com>
2020-05-31 20:17:23 +0900

    refactor: use async/await and fix some

--- tasks/image.js
+++ tasks/image.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
-const { promisify } = require('util');
+const {promisify} = require('util');
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const eachAsync = require('each-async');
 const chalk = require('chalk');
 const filesize = require('filesize');
-const { round10 } = require('round10');
+const {round10} = require('round10');
 const optimize = require('../lib/optimize');
 
 const readFile = promisify(fs.readFile);
@@ -26,46 +26,43 @@ module.exports = function (grunt) {
       svgo: true
     });
 
-    eachAsync(this.files, (file, index, next) => {
+    eachAsync(this.files, async (file, index, next) => {
       const src = file.src[0];
-      const dest = file.dest;
+      const {dest} = file;
 
-      // make directory if does not exist
-      mkdirp.sync(path.dirname(dest));
+      try {
+        // Make directory if does not exist
+        mkdirp.sync(path.dirname(dest));
 
-      readFile(src)
-        .then(buffer => optimize(buffer, options))
-        .then(buffer => {
-          let original = fs.statSync(src).size;
-          let diff = original - buffer.length;
-          let diffPercent = round10(100 * (diff / original), -1);
+        const originalBuffer = await readFile(src);
+        const originalSize = originalBuffer.length;
+        const optimizedBuffer = await optimize(originalBuffer, options);
+        const optimizedSize = optimizedBuffer.length;
 
-          if (diff <= 0) {
-            return readFile(src)
-              .then(buffer => writeFile(dest, buffer))
-              .then(() => {
-                grunt.log.writeln(
-                  chalk.green('- ') + file.src + chalk.gray(' ->') +
-                  chalk.gray(' Cannot improve upon ') + chalk.cyan(filesize(original))
-                );
-              });
-          } else {
-            return writeFile(dest, buffer)
-              .then(() => {
-                grunt.log.writeln(
-                  chalk.green('✔ ') + file.src + chalk.gray(' ->') +
-                  chalk.gray(' before=') + chalk.yellow(filesize(original)) +
-                  chalk.gray(' after=') + chalk.cyan(filesize(buffer.length)) +
-                  chalk.gray(' reduced=') + chalk.green.underline(filesize(diff) + '(' + diffPercent + '%)')
-                );
-              });
-          }
-        })
-        .then(() => next())
-        .catch(error => {
-          grunt.warn(error);
-          next(error);
-        });
+        const diffSize = originalSize - optimizedSize;
+        const diffPercent = round10(100 * (diffSize / originalSize), -1);
+
+        if (diffSize <= 0) {
+          grunt.log.writeln(
+            chalk.green('- ') + file.src + chalk.gray(' ->') +
+            chalk.gray(' Cannot improve upon ') + chalk.cyan(filesize(originalSize))
+          );
+        } else {
+          await writeFile(dest, optimizedBuffer);
+
+          grunt.log.writeln(
+            chalk.green('✔ ') + file.src + chalk.gray(' ->') +
+              chalk.gray(' before=') + chalk.yellow(filesize(originalSize)) +
+              chalk.gray(' after=') + chalk.cyan(filesize(optimizedSize)) +
+              chalk.gray(' reduced=') + chalk.green.underline(filesize(diffSize) + '(' + diffPercent + '%)')
+          );
+        }
+
+        next();
+      } catch (error) {
+        grunt.warn(error);
+        next(error);
+      }
     }, error => {
       if (error) {
         grunt.warn(error);
